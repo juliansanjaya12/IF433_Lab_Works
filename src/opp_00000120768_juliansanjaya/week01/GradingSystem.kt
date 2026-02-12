aee3e3fa02a8c6b606a2d4d5ec4bce08b9900e54
juliansanjaya12 <juliansanjaya16@gmail.com>
2026-02-12 21:01:50 +0700

    week01: initial setup and procedural code

---
+++ src/opp_00000120768_juliansanjaya/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package opp_00000120768_juliansanjaya.week01
+
+fun main() {
+    var name: String = "Julian Sanjaya"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
