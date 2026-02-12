5884413d4a70ff1465de7a53c378a3d1151d0cfb
juliansanjaya12 <juliansanjaya16@gmail.com>
2026-02-12 21:07:59 +0700

    week01: refactor to use val and string templates

--- src/opp_00000120768_juliansanjaya/week01/GradingSystem.kt
+++ src/opp_00000120768_juliansanjaya/week01/GradingSystem.kt
@@ -1,8 +1,15 @@
 package opp_00000120768_juliansanjaya.week01
 
-fun main() {
+/*fun main() {
     var name: String = "Julian Sanjaya"
     var score: Int = 80
 
     println("Nama: " + name + ", Nilai: " + score)
+}*/
+
+fun main() {
+    val name = "Julian Sanjaya"
+    val score = 80
+
+    println("Nama: $name, Nilai: $score")
 }
