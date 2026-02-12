61fa2c6804f716aed9b36430cff51f6fbc5893eb
juliansanjaya12 <juliansanjaya16@gmail.com>
2026-02-12 22:06:22 +0700

    week01: (tugas 2) implement discount logic

--- src/opp_00000120768_juliansanjaya/week01/GameStore.kt
+++ src/opp_00000120768_juliansanjaya/week01/GameStore.kt
@@ -4,3 +4,7 @@ fun main() {
     val gameTitle = "Nenek Ku Seorang Gamer - Simulator"
     val price = 650_000
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) price * 20 / 100
+    else price * 10 / 100
