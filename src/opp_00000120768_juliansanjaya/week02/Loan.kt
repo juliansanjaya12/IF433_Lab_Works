fb76f287a51bbfabc8d88383ccbc790908652e19
juliansanjaya12 <juliansanjaya16@gmail.com>
2026-02-13 16:07:25 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/opp_00000120768_juliansanjaya/week02/Loan.kt
@@ -0,0 +1,17 @@
+package opp_00000120768_juliansanjaya.week02
+
+import java.util.Scanner
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
