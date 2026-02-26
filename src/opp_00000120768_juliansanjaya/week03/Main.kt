55374f730bf79b33b40fedf0133ac957a96eb55b
juliansanjaya12 <juliansanjaya16@gmail.com>
2026-02-26 23:03:48 +0700

    week03: (task 1) implement Weapon testing in main

--- src/opp_00000120768_juliansanjaya/week03/Main.kt
+++ src/opp_00000120768_juliansanjaya/week03/Main.kt
@@ -1,6 +1,6 @@
 package opp_00000120768_juliansanjaya.week03
 
-fun main() {
+/*fun main() {
     val e = Employee("Budi")
 
     e.salary = -1000
@@ -9,4 +9,17 @@ fun main() {
 
     e.increasePerformance()
     println("Pajak yang harus dibayar: ${e.tax}")
+}*/
+
+fun main() {
+    val w = Weapon("Excalibur", 100)
+
+    // harus gagal (tidak berubah)
+    w.damage = -50
+
+    // harus dipaksa jadi 1000
+    w.damage = 9999
+
+    println("Damage akhir: ${w.damage}")
+    println("Tier senjata: ${w.tier}")
 }
