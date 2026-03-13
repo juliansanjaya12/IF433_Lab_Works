835a57056715af017b697353e57ea2b76551fc14
juliansanjaya12 <juliansanjaya16@gmail.com>
2026-03-13 23:06:13 +0700

    week06: (task) instantiate smart devices in main

--- src/opp_00000120768_juliansanjaya/week06/Main.kt
+++ src/opp_00000120768_juliansanjaya/week06/Main.kt
@@ -1,6 +1,6 @@
 package opp_00000120768_juliansanjaya.week06
 
-fun processCheckout(method: PaymentMethod, amount: Double){
+/*fun processCheckout(method: PaymentMethod, amount: Double){
     println("-> Memulai Checkout...")
     method.pay(amount)
 }
@@ -18,4 +18,32 @@ fun main(){
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(pay1, 50000.0)
     processCheckout(pay2, 150000.0)
+}*/
+
+fun main() {
+
+    println("=== SMART HOME SYSTEM ===")
+
+    val lamp = SmartLamp("L001", "Lampu Ruang Tamu")
+    val speaker = SmartSpeaker("S001", "Google Nest Dapur")
+    val cctv = SmartCCTV("C001", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+
+    println("\nMenambahkan perangkat ke Smart Home Hub...")
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("\n=== Menghidupkan semua perangkat ===")
+    lamp.turnOn()
+    speaker.turnOn()
+    cctv.turnOn()
+
+    println("\n=== Mengaktifkan Mode Keamanan ===")
+    hub.activateSecurityMode()
+
+    println("\n=== Mematikan Semua Perangkat Switchable ===")
+    hub.turnOffAllSwitches()
+
 }
