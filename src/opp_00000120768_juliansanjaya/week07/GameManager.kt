25d4e1d731f24b37036ee6d06d05bd8f06ea0b74
juliansanjaya12 <juliansanjaya16@gmail.com>
2026-04-03 23:07:11 +0700

    week07: (task) create GameManager singleton

---
+++ src/opp_00000120768_juliansanjaya/week07/GameManager.kt
@@ -0,0 +1,14 @@
+package opp_00000120768_juliansanjaya.week07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
