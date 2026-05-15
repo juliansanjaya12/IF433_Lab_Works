69737d76cf5de88e3642293059220d1532b1cf67
julian <juliansanjaya16@gmail.com>
2026-05-15 18:13:22 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/opp_00000120768_juliansanjaya/week10/ WalletModels.kt
@@ -0,0 +1,11 @@
+package opp_00000120768_juliansanjaya.week10
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
