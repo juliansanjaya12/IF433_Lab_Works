9591ffa6836230089d7bd02cbe0d1e5c59abab94
julian <juliansanjaya16@gmail.com>
2026-05-15 22:31:08 +0700

    week11: create string extension functions

---
+++ src/opp_00000120768_juliansanjaya/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package opp_00000120768_juliansanjaya.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
